836fe2491b4d80e8a4e9e538a80f3ca28279c5d9
ghuellll <ghuelteves@gmail.com>
2024-12-19 06:29:51 +0800

    changes

--- manifests/monitor.pp
+++ manifests/monitor.pp
@@ -3,7 +3,7 @@
 class puppetmonitor{
 
  package { ['vim', 'curl', 'git']:
-  ensure =>installed,
+  ensure => installed,
  }
 
  user { 'monitor':
@@ -18,4 +18,12 @@ class puppetmonitor{
    owner => 'monitor',
    group => 'monitor',
    mode  => '0755',
- }   
+ }
+ exec { 'download_memory_check':
+   command => '/usr/bin/curl -o /home/monitor/scripts/memory_check https://raw.githubusercontent/ghuellll/part2/master/memory_check',
+   creates => '/home/monitor/scripts/memory_check',
+   user    => 'monitor',
+   group   => 'monitor',
+   require =>  File['/home/monitor/scripts'],
+ } 
+}   
